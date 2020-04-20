15b2cf5ce30260c2fddb2c38a9caedd15932756d
thedavdo <david.baker542@gmail.com>
2020-04-20 15:29:56 -0400

    Updated folder structure
    game package added
    android package added

--- TeamProjectGame/app/src/main/java/com/cnit/teamproject/android/MainActivity.java
+++ TeamProjectGame/app/src/main/java/com/cnit/teamproject/android/MainActivity.java
@@ -1,8 +1,10 @@
-package com.cnit.teamproject;
+package com.cnit.teamproject.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.cnit.teamproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
