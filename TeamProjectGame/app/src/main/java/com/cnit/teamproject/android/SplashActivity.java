3761b701c9a1c1d997b66fc39268f0ea8eaec5c1
lindera1 <31320455+lindera1@users.noreply.github.com>
2020-05-04 00:29:02 -0400

    -- Final merging animation completed.

--- TeamProjectGame/app/src/main/java/com/cnit/teamproject/android/SplashActivity.java
+++ TeamProjectGame/app/src/main/java/com/cnit/teamproject/android/SplashActivity.java
@@ -11,7 +11,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_main_menu);
     }
 }
 //im a comment
