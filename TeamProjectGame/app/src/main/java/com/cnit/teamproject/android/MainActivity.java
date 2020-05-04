0ba56f2e331d062b2f4e72ff3bd5e1e61be198e5
thedavdo <david.baker542@gmail.com>
2020-05-04 09:01:57 -0400

    Added functionality for BackPressListener

--- TeamProjectGame/app/src/main/java/com/cnit/teamproject/android/MainActivity.java
+++ TeamProjectGame/app/src/main/java/com/cnit/teamproject/android/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cnit.teamproject.android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -7,6 +8,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -21,6 +23,7 @@ import com.cnit.teamproject.R;
 
 public class MainActivity extends AppCompatActivity implements MainMenuFragment.Callbacks {
 
+
     private static int SPLASHSCREENDELAY = 4000; //in MS clearly.
 
     @Override
@@ -31,28 +34,68 @@ public class MainActivity extends AppCompatActivity implements MainMenuFragment.
 
         Fragment frag = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
 
-        if(frag == null) {
+        if(savedInstanceState != null) {
+            //TODO
+        }
+        else if(frag == null) {
+
             frag = new SplashFragment();
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, frag).commit();
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, frag)
+                    .commit();
 
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     MainMenuFragment mmFrag = new MainMenuFragment();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, mmFrag).commit();
+
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, mmFrag)
+                            .commit();
+
                 }
             }, SPLASHSCREENDELAY);
         }
     }
 
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
     @Override
     public void onStartPress() {
+
         GameFragment gameFrag = new GameFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, gameFrag).commit();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, gameFrag)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Fragment fr = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+
+        boolean consumed = true;
+
+        if(fr instanceof BackPressedListener) {
+            consumed = ((BackPressedListener) fr).onBackPressed();
+        }
+
+        if(!consumed) super.onBackPressed();
     }
 
     @Override
     public void onExitPress() {
+
         finish();
     }
 }
